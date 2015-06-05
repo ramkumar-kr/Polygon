cded957dafb38da759780c0be08278a097c7d154
Ram Kumar K R <ramkumar.kr@urbanladder.com>
2015-06-05 12:24:33 +0530

    Added spaceship operator and addition of two length objects

--- lib/polygon/length.rb
+++ lib/polygon/length.rb
@@ -1,12 +1,27 @@
 # Represents the measure of a side in a Polygon
 class Polygon::Length
 	attr_reader :value
-	
+	METER = 1000
+	CENTIMETER = 10
+	MILLIMETER = 1
+	include Comparable
+
 	def initialize(length,unit)
-		@value = length * 1000 if unit == 'm'
-		@value = length * 10 if unit == 'cm'
-		@value = length if unit == 'mm'
+		@unit = unit
+		@value = length
 	end
 
+	def in_millimeters
+		@unit * @value
+	end
+
+	def <=>(other_length)
+		return nil if other_length.class != self.class
+		in_millimeters <=> other_length.in_millimeters
+	end
+
+	def +(other_length)
+		Polygon::Length.new(other_length.in_millimeters + in_millimeters, MILLIMETER)
+	end
 
 end
--- spec/length_spec.rb
+++ spec/length_spec.rb
@@ -2,9 +2,47 @@ require 'spec_helper'
 
 describe Polygon::Length do 
 
-	it "should return the length of the length in mm" do
-		length1 = Polygon::Length.new(6,'m')
-		expect(length1.value).to eq(6000)
+	context 'Equality' do
+
+		it "should return 1 when 690 centimeters is compared with 6.45 meters" do
+			length1 = Polygon::Length.new(6.45, Polygon::Length::METER)
+			length2 = Polygon::Length.new(690, Polygon::Length::CENTIMETER)
+			expect(length2 <=> length1).to eq(1)
+		end
+
+		it "should return 0 when 645 centimeters is compared with 6.45 meters" do
+			length1 = Polygon::Length.new(6.45, Polygon::Length::METER)
+			length2 = Polygon::Length.new(645, Polygon::Length::CENTIMETER)
+			expect(length2 <=> length1).to eq(0)
+		end
+
+		it "should return -1 when 640 centimeters is compared with 6.45 meters" do
+			length1 = Polygon::Length.new(6.45, Polygon::Length::METER)
+			length2 = Polygon::Length.new(640, Polygon::Length::CENTIMETER)
+			expect(length2 <=> length1).to eq(-1)
+		end
+
+		it "should return nil when 690 centimeters is compared with an Object" do
+			length1 = Object.new
+			length2 = Polygon::Length.new(690, Polygon::Length::CENTIMETER)
+			expect(length2 <=> length1).to eq(nil)
+		end
+	end
+
+	context 'in_millimeters' do
+		it "should return 640 for a length object initialized with 64 centimeters " do
+			length = Polygon::Length.new(64, Polygon::Length::CENTIMETER)
+			expect(length.in_millimeters).to eq(640)
+		end
+	end
+
+	context 'Addition' do
+		it "should return a length object of 60 centimeters when a length object of 36 centimeters is added to another length object of 240 millimeters" do
+			length1 = Polygon::Length.new(36, Polygon::Length::CENTIMETER)
+			length2 = Polygon::Length.new(240, Polygon::Length::MILLIMETER)
+			length3 = Polygon::Length.new(60, Polygon::Length::CENTIMETER)
+			expect(length1 + length2).to eq(length3)
+		end
 	end
 
 end 
